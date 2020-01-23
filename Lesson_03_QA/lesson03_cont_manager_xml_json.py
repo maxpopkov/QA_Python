c2bf5cdbadd7d46423142880c3d8143ace990842
Max P <maxpopkovvv123@gmail.com>
2020-01-23 09:54:39 +0300

    Lesson_03: Чтение и запись файлов csv, json(Поправил чтение)

--- Lesson_03_QA/lesson03_cont_manager_xml_json.py
+++ Lesson_03_QA/lesson03_cont_manager_xml_json.py
@@ -2,38 +2,41 @@ import json
 import csv
 
 """Читаем файл user.json"""
-path_json = "users.json"
-with open(path_json, "r") as read_file_json:
-    data_json = json.load(read_file_json)
-
-"""Читаем файл books.csv"""
-path_csv = "books.csv"
-results_csv = []
-with open(path_csv, "r") as read_file_csv:
-    data_csv = csv.DictReader(read_file_csv)
-    for row in data_csv:
-        """Получаем упорядочный словарь"""
-        results_csv.append(row)
-print(results_csv)
-
 name_list = []
 gender_list = []
 address_list = []
+path_json = "users.json"
 
-"""Собираем списки данных"""
-for id_dict in data_json:
-    name = id_dict["name"]
-    name_list.append(name)
-    gender = id_dict["gender"]
-    gender_list.append(gender)
-    address = id_dict["address"]
-    address_list.append(address)
+with open(path_json, "r") as read_file_json:
+    data_json = json.load(read_file_json)
+    for id_dict in data_json:
+        name = id_dict["name"]
+        name_list.append(name)
+        gender = id_dict["gender"]
+        gender_list.append(gender)
+        address = id_dict["address"]
+        address_list.append(address)
 
-user_dict = {}
+"""Читаем файл books.csv"""
+dic_book={}
+dic_book_list=[]
+path_csv = "books.csv"
+
+with open(path_csv, "r") as read_file_csv:
+    data_csv = csv.DictReader(read_file_csv)
+    for row in data_csv:
+        dic_book = {"Title": row["Title"], "Author": row["Author"]}
+        dic_book_list.append(dic_book)
+
+
+
+too_json = {}
 too_json_list = []
 """Собираем словарь для передачи в json"""
-for number in range(len(name_list)):
-    too_json = {"name ": name_list[number], "gender: ": gender_list[number], "address ": address_list[number]}
+for number_1 in range(len(name_list)):
+    too_json = {"name ": name_list[number_1], "gender: ": gender_list[number_1], "address ": address_list[number_1]}
+    for number in range(len(dic_book_list)):
+        too_json["book"]=dic_book_list[number]
     too_json_list.append(too_json)
 
 """Пишем в файл созданный словарь"""
